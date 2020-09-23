58b305e8accf2d78c77569942a35e2894e789045
ihtadalutfi <71589785+ihtadalutfi@users.noreply.github.com>
2020-09-22 20:19:49 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int jumlah = 10000;
+        int hari= 30;
+        int perhitungan = jumlah * hari;
+        System.out.println(perhitungan);
+    }
+}
